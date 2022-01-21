0efba35a88bb2e0a12cd127380d8ec8e53205c4a
mj-support <marco-hunter@web.de>
2022-01-21 15:15:09 +0100

    flexible batching working

--- src/next.py
+++ src/next.py
@@ -19,24 +19,27 @@ def delivery_report(err, msg):
 
 
 # data_base_path = "../DS"
-data_base_path = "/home/eschulze/Projects/LID-DS-2021-fixed-exploit-time"
-scenario_names = os.listdir(data_base_path)
-scenario_path_example = os.path.join(data_base_path, scenario_names[0])
+#data_base_path = "/home/eschulze/Projects/LID-DS-2021-fixed-exploit-time"
+#scenario_names = os.listdir(data_base_path)
+#scenario_path_example = os.path.join(data_base_path, scenario_names[0])
+test_path_marco = "/home/scadspc14/PycharmProjects/CVE-2017-7529"
 
-dataloader = dataloader_factory(scenario_path_example, direction=Direction.BOTH)
+dataloader = dataloader_factory(test_path_marco, direction=Direction.BOTH)
 data_type_list = iter([dataloader.training_data(), dataloader.validation_data(), dataloader.test_data()])
 recs_of_current_type = iter(next(data_type_list))
 syscalls_of_current_rec = next(recs_of_current_type).syscalls()
 current_sys = next(syscalls_of_current_rec)
 current_timestamp = current_sys.timestamp_unix_in_ns()
+
 t_0 = time.time_ns()
+last_timestamp = current_timestamp
 
 while True:
     syscall_batch = []
     t_help = time.time_ns()
     t_delta = t_help - t_0
 
-    while current_timestamp <= t_delta + current_timestamp:
+    while current_timestamp <= t_delta + last_timestamp:
         syscall_batch.append(current_sys.syscall_line)
         try:
             current_sys = next(syscalls_of_current_rec)
@@ -53,23 +56,17 @@ while True:
                 current_timestamp = current_sys.timestamp_unix_in_ns()
 
     if len(syscall_batch) > 0:
-
-        #sending in batches rather than separately?
+        print("hurray")
         for syscall in syscall_batch:
             print(syscall)
+            """p.poll(0)
+               p.produce("test", syscall.encode("utf-8"), callback=delivery_report)
+        p.flush()"""
 
+    last_timestamp = current_timestamp
     t_0 = t_help
 
 
 
 
-"""for data in data_types:
-        for recording in data:
-            for syscall in recording.syscalls():
-
-                p.poll(0)
-    
-                p.produce("test", syscall.syscall_line.encode('utf-8'), callback=delivery_report)
-    
-    p.flush()"""
 
